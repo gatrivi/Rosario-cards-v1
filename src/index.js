187136a41ffcd6893e17e824767b487938eff0fc
Gatrivi <62384085+gatrivi@users.noreply.github.com>
2026-08-14 23:43:14 -0300

    Bump app version to 0.3.80

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { subscribeToAppUpdates } from './utils/appUpdate';
 import { installNovenaAutoProgress } from './utils/novenaAutoProgress';
 import { installErrorCapture } from './utils/errorCapture';
 
-const APP_VERSION = '0.3.79';
+const APP_VERSION = '0.3.80';
 
 installNovenaAutoProgress();
 installErrorCapture();
